b65b24419b00372179efacbf7079bf1592cf8a7a
mahmud035 <mahamudulhasan4148@gmail.com>
2022-03-08 16:07:49 +0600

    lecture 02

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 //* -----42-1 Six JavaScript Fundamentals that you need to know-----
 
-// 1. How to declare a variable using let and const
+/* // 1. How to declare a variable using let and const
 const fatherName = 'Arnold';
 let season = 'Summer';
 season = 'winter';
@@ -47,4 +47,44 @@ const student = {
 const myVariable = 'age';
 console.log(student.age); // direct by property
 console.log(student['age']); // access via property name string
-console.log(student[myVariable]); // access via property name in a variable
+console.log(student[myVariable]); // access via property name in a variable */
+
+//* -----42-2 template string, arrow function, spread operator-----
+
+/* // 1. template string
+const numbers = [89, 35, 98, 12];
+
+const student = {
+  name: 'Skib Khan',
+  age: 34,
+  movies: ['King khan', 'Dhakar Mastan'],
+};
+
+const about = `My name is ${student.name}. I am ${student.age} years old. Has number: ${numbers[2]}. Also liked movies: ${student.movies[1]}, ${student.movies[0]}.`;
+console.log(about);
+
+// 2. arrow function
+const getFiftyFive = () => 55;
+
+const addSixtyFive = (num) => num + 65;
+
+const isEven = (x) => x % 2 === 0;
+
+const addThree = (x, y, z) => x + y + z;
+
+const doMath = (num1, num2) => {
+  const sum = num1 + num2;
+  return sum;
+};
+
+// 3. spread operator
+console.log(numbers);
+const newNumbers = [77, ...numbers, 999];
+numbers.push(100);
+
+console.log(newNumbers);
+console.log(numbers);
+
+//* create a new array from an older array and add an element
+const currentNumbers = [...numbers, 55];
+console.log(currentNumbers); */

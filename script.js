ebb2ba90daf18a1f4964327d02ef10aaf80437ad
mahmud035 <mahamudulhasan4148@gmail.com>
2022-03-08 18:00:10 +0600

    lecture 04

--- script.js
+++ script.js
@@ -122,3 +122,59 @@ console.log(specificName);
 // 4. find()
 const special = products.find((product) => product.name.includes('n'));
 console.log(special); */
+
+//* -----42-4 Array and object Destructuring, optional chaining-----
+
+// 1. array destructuring
+const numbers = [42, 65];
+// const x = numbers[0];
+// const y = numbers[1];
+
+const [x, y] = [42, 65];
+const [p, q] = numbers;
+console.log(x, y);
+
+function boxify(num1, num2) {
+  const nums = [num1, num2];
+  return nums;
+}
+
+const [first, second] = boxify(90, 34);
+// console.log(boxify(90, 34));
+console.log(first, second);
+
+const student = {
+  name: 'Skib Khan',
+  age: 34,
+  movies: ['King khan', 'Dhakar Mastan'],
+};
+
+const [firstMovie, secondMovie] = student.movies;
+console.log(firstMovie, secondMovie);
+
+// 2. object destructuring
+const { name, age } = { name: 'alu', age: 14 };
+// const { name, age } = { id: 12, name: 'alu', salary: 3400, age: 14 };
+
+const employee = {
+  ide: 'VS Code',
+  designation: 'developer',
+  machine: 'mac',
+  languages: ['html', 'css', 'js'],
+  specification: {
+    height: 66,
+    weight: 67,
+    address: 'kumarkhali',
+    drink: 'water',
+    watch: {
+      color: 'black',
+      price: 500,
+      brand: 'garmin',
+    },
+  },
+};
+
+const { machine, ide } = employee;
+const { weight, address } = employee.specification;
+const { brand } = employee?.specification?.watch;
+console.log(brand);

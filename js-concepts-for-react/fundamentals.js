b8d52878dc655f43ca55b73b7600f145cb9d56ff
mahmud035 <mahamudulhasan4148@gmail.com>
2022-03-08 15:31:06 +0600

    lecture 1

--- js-concepts-for-react/fundamentals.js
+++ js-concepts-for-react/fundamentals.js
@@ -1,50 +1,47 @@
-'use strict';
-
-//* -----42-1 Six JavaScript Fundamentals that you need to know-----
-
 // 1. How to declare a variable using let and const
 const fatherName = 'Arnold';
-let season = 'Summer';
+let season = 'rainy';
 season = 'winter';
 
 // 2. conditions
-// 6 basic conditions: >, <, ===, !==. <=, >=
+// 6 basic conditions: >,<, ===, !==, <=, >=
 // multiple conditions: &&, ||
 
 if (fatherName === 'arnold' || season === 'rainy') {
+} else if (fatherName === 'Arnold') {
 } else {
 }
 
 // 3. array declare
-// index
+// index,
 // length, push,
 const numbers = [89, 35, 98, 12];
 numbers[0] = 56;
 
-// 4. for loop, while loop
-// for loop
+// 4. for loop
 for (let i = 0; i < numbers.length; i++) {
   const number = numbers[i];
-  console.log(number);
+  confirm.log(number);
 }
 
 // 5. function
-function multiply(a, b) {
-  const result = a * b;
+function multiply(num1, num2) {
+  const result = num1 * num2;
   return result;
 }
 
-const output = multiply(90, 10);
+const output = multiply(35, 78);
 
 // 6. Object
-//* 3 ways to access object property by name
+//* 3 ways to access property by name
 const student = {
-  name: 'John',
-  age: 34,
-  movies: ['King khan', 'Dhakar Mastan'],
+  name: 'Salib Khan',
+  age: 32,
+  movies: ['king khan', 'Dhakar Mastan'],
 };
 
 const myVariable = 'age';
+
 console.log(student.age); // direct by property
 console.log(student['age']); // access via property name string
 console.log(student[myVariable]); // access via property name in a variable
